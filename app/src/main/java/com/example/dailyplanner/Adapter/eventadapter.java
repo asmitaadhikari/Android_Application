36f1b11a7636c5b65973b42e5d721d7005e95886
asmitaadhikari <asmitaadhikari62@gmail.com>
2020-01-30 17:38:16 +0545

    Event Model Created

--- app/src/main/java/com/example/dailyplanner/Adapter/eventadapter.java
+++ app/src/main/java/com/example/dailyplanner/Adapter/eventadapter.java
@@ -1,11 +1,62 @@
 package com.example.dailyplanner.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class eventadapter
+
+import com.example.dailyplanner.Model.event;
+import com.example.dailyplanner.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class eventadapter extends RecyclerView.Adapter<eventadapter.ViewHolder>
+
 {
+    private Context context;
+    private ArrayList<event> eventlist;
+
+    public eventadapter(Context context,ArrayList<event> eventlist)
+    {
+        this.context=context;
+        this.eventlist=eventlist;
+    }
 
 
+//    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_showlist,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        final event Event=eventlist.get(position);
+
+       
+
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }

1a14e90b72d8ead4050f34d1fbf6e0e80366ff77
asmitaadhikari <asmitaadhikari62@gmail.com>
2020-01-31 14:36:07 +0545

    adapter completed

--- app/src/main/java/com/example/dailyplanner/Activity/Alarm.java
+++ app/src/main/java/com/example/dailyplanner/Activity/Alarm.java
@@ -1,19 +1,134 @@
 package com.example.dailyplanner.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.dailyplanner.Add_Alarm;
 import com.example.dailyplanner.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class Alarm extends AppCompatActivity {
 
     private TextView txtTime;
 
+    private TextView txtDate;
+
+    private int year, month, day;
+    private int hour, min;
+
+    private String week_days = "";
+
+    private int type;
+
+    private TextView edtLocation;
+
+    private EditText edtTitle, edtDescription;
+
+    private Boolean updateEvent = false;
+    private int id;
+
+    private AppCompatSpinner spnType, spnNotification;
+
+    private TextView txtAM_PM;
+
+
+    private TextView txtMon, txtTue, txtWed, txtThr, txtFri, txtSat, txtSun;
+
+    private int mon, tue, wed, thr, fri, sat, sun;
+    private CardView weekLayout;
+
+    private int request_code;
+
+    private Calendar selectedCalendar;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm);
+        setContentView(R.layout.activity_dashboard);
+
+      weekLayout=findViewById(R.id.weekLayout);
+      weekLayout.setVisibility(View.GONE);
+
+        mon = tue = wed = thr = fri = sat = sun = 0;
+
+        txtMon = findViewById(R.id.txtMon);
+        txtTue = findViewById(R.id.txtTue);
+        txtWed = findViewById(R.id.txtWed);
+        txtThr = findViewById(R.id.txtThr);
+        txtFri = findViewById(R.id.txtFri);
+        txtSat = findViewById(R.id.txtSat);
+        txtSun = findViewById(R.id.txtSun);
+
+        txtAM_PM = findViewById(R.id.txtAM_PM);
+
+        spnType = findViewById(R.id.spnCategory);
+        spnNotification = findViewById(R.id.spnNotification);
+
+        List<String> typeList = new ArrayList<>();
+        typeList.add("Once");
+        typeList.add("Weekly");
+        typeList.add("Monthly");
+        typeList.add("Yearly");
+        typeList.add("Repeat So on");
+        typeList.add("Custom");
+
+
+        List<String> notifyList = new ArrayList<>();
+        notifyList.add("On time");
+        notifyList.add("30 min before");
+
+        ArrayAdapter typeAdapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,typeList);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnType.setAdapter(typeAdapter);
+
+        spnType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l)
+            {
+                if(spnType.getSelectedItem().toString().equals("Custom"))
+                {
+                   startActivity(new Intent(Alarm.this, Add_Alarm.class));
+                   weekLayout.setVisibility(View.GONE);
+                }else if(spnType.getSelectedItem().toString().equals("Once"))
+                    weekLayout.setVisibility(View.GONE);
+                else
+                {
+                    weekLayout.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                        public void onNothingSelected(AdapterView<?> adapterView)
+                {
+                    if(spnType.getSelectedItem().toString().equals("Custom"))
+                    {
+                        startActivity(new Intent(Alarm.this,Add_Alarm.class));
+                        weekLayout.setVisibility(View.GONE);
+
+                    }else if (spnType.getSelectedItem().toString().equals("Once");
+                }
+
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/dailyplanner/Adapter/eventadapter.java
+++ app/src/main/java/com/example/dailyplanner/Adapter/eventadapter.java
@@ -1,69 +1,176 @@
 package com.example.dailyplanner.Adapter;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.view.menu.MenuPopupHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import com.example.dailyplanner.Activity.Alarm;
+import com.example.dailyplanner.Activity.Dashboard;
 import com.example.dailyplanner.Model.event;
 import com.example.dailyplanner.R;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class eventadapter extends RecyclerView.Adapter<eventadapter.ViewHolder>
-
-{
+public class eventadapter extends RecyclerView.Adapter<eventadapter.ViewHolder> {
     private Context context;
     private ArrayList<event> eventlist;
 
-    public eventadapter(Context context,ArrayList<event> eventlist)
-    {
-        this.context=context;
-        this.eventlist=eventlist;
+    public eventadapter(Context context, ArrayList<event> eventlist) {
+        this.context = context;
+        this.eventlist = eventlist;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType)
-    {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_showevent,parent,false);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_showevent, parent, false);
         return new ViewHolder(view);
 
     }
 
     @Override
-    public void onBindViewHolder( ViewHolder holder,final int position)
-    {
-        final event Event=eventlist.get(position);
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final event Event = eventlist.get(position);
+
+        holder.txtTitle.setText(event.getTitle());
+        holder.txtDescription.setText(event.getDescription());
+        holder.txtTime.setText(event.getTime());
+        holder.txtLocation.setText(event.getLocation());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context, Dashboard.class);
+
+                intent.putExtra("title", event.getTitle());
+                intent.putExtra("description", event.getDescription());
+                String time[] = event.getTime().split(" ");
+                intent.putExtra("time", time[0]);
+                intent.putExtra("am_pm", time[1]);
+                intent.putExtra("location", event.getLocation());
+
+                context.startActivity(intent);
+            }
+        });
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public String getMonthName(String month) {
+        switch (month) {
+            case "01":
+                return "Jan";
+            case "02":
+                return "Feb";
+            case "03":
+                return "May";
+            case "04":
+                return "Apr";
+            case "05":
+                return "May";
+            case "06":
+                return "Jun";
+            case "07":
+                return "Jul";
+            case "08":
+                return "Aug";
+            case "09":
+                return "Sept";
+            case "10":
+                return "Oct";
+            case "11":
+                return "Nov";
+            case "12":
+                return "Dec";
+            default:
+                return "";
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return eventlist.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView txtTitle,txtDescription,txtTime,txtDate,txtLocation;
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtDescription = itemView.findViewById(R.id.txtDescription);
+            txtTime = itemView.findViewById(R.id.txtTime);
+            txtDate = itemView.findViewById(R.id.txtDate);
+            txtLocation = itemView.findViewById(R.id.txtLocation);
         }
     }
+
+    @SuppressLint("RestrictedAPI")
+    public void onMenuBtn(View view, final event Event, final int position)
+    {
+        MenuBuilder menuBuilder=new MenuBuilder(context);
+        MenuInflater inflater =new MenuInflater(context);
+        inflater.inflate(R.menu.list_menu,menuBuilder);
+        MenuPopupHelper optionsMenu=new MenuPopupHelper(context,menuBuilder,view);
+        optionsMenu.setForceShowIcon(true);
+
+        menuBuilder.setCallback(new MenuBuilder.Callback() {
+            @Override
+            public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.opt_edit:
+                        Intent intent=new Intent(context, Alarm.class);
+
+                        intent.putExtra("id", event.getId());
+                        intent.putExtra("title",event.getTitle());
+                        intent.putExtra("description", event.getDescription());
+                        String time[] = event.getTime().split(" ");
+                        intent.putExtra("time", time[0]);
+                        intent.putExtra("am_pm", time[1]);
+                        intent.putExtra("location", event.getLocation());
+
+                        context.startActivity(intent);
+
+                        break;
+                    case R.id.opt_delete:
+                        AlertDialog.Builder builder;
+                        if(Build.VERSION.SDK_INT > = Build.VERSION_CODES.LOLLIPOP )
+                        {
+                            builder=new AlertDialog.Builder(context,android.R.style.Theme_Material_Dialog_Alert)
+
+                        }else
+                        {
+                            builder=new AlertDialog.Builder(context);
+                        }
+                        builder.setTitle("Delete Entry")
+                                .setMessage("Are you sure you want to delete this event?")
+                                .setPositiveButton(android.R.string.yes,Event)
+
+                }
+            }
+
+            @Override
+            public void onMenuModeChange(MenuBuilder menu) {
+
+            }
+        });
+    }
+
+
 }

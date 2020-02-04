0cc924099089e1744c697cc2a89e767d5bc945ad
asmitaadhikari <asmitaadhikari62@gmail.com>
2020-02-04 11:28:13 +0545

    Date is displayed\

--- app/src/main/java/com/example/dailyplanner/Activity/Dashboard.java
+++ app/src/main/java/com/example/dailyplanner/Activity/Dashboard.java
@@ -11,63 +11,73 @@ import android.widget.Toast;
 
 import com.example.dailyplanner.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Dashboard extends AppCompatActivity {
-    private TextView txtDate,txtTime,txtlocation,txtAM_PM;
-    private EditText edTittle,edtDescription;
+    private TextView txtDate, txtTime, txtlocation, txtAM_PM;
+    private EditText edTittle, edtDescription;
 
-    private Spinner spnCategory,spnNotification;
-
-
+    private Spinner spnCategory, spnNotification;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_dashboard);
 
-        spnCategory=findViewById(R.id.spnCategory);
+        String date_n = java.text.DateFormat.getDateTimeInstance().format(new Date());
 
-        List<String> categories=new ArrayList<>();
-        categories.add("Once");
-        categories.add("Daily");
-        categories.add("Weekly");
-        categories.add("Yearly");
-        categories.add("So on");
-        categories.add("Custom");
+        //get hold of textview.
+        TextView date = findViewById(R.id.txtTime);
 
-        ArrayAdapter adapter1=new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,categories);
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //set it as current date.
+        date.setText(date_n);
 
-        spnCategory.setAdapter(adapter1);
-        spnCategory.setEnabled(false);
+//
+//        spnCategory = findViewById(R.id.spnCategory);
+//
+//        List<String> categories = new ArrayList<>();
+//        categories.add("Once");
+//        categories.add("Daily");
+//        categories.add("Weekly");
+//        categories.add("Yearly");
+//        categories.add("So on");
+//        categories.add("Custom");
+//
+//        ArrayAdapter adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+//        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        txtDate =findViewById(R.id.date_or_days);
-        txtTime=findViewById(R.id.txtTime);
-
-        edTittle=findViewById(R.id.edtTitle);
-        edtDescription=findViewById(R.id.edDescription);
-        txtlocation=findViewById(R.id.edtLocation);
-
-        try
-        {
-            if(!getIntent().getExtras().getString("date").equals(""))
-            {
-                edTittle.setText(getIntent().getExtras().getString("title"));
-                edtDescription.setText(getIntent().getExtras().getString("description"));
-                txtTime.setText(getIntent().getExtras().getString("time"));
-                txtAM_PM.setText(getIntent().getExtras().getString("am_pm"));
-                txtlocation.setText(getIntent().getExtras().getString("location"));
-                txtDate.setText(getIntent().getExtras().getString("date"));
-            }
-        }
-        catch (Exception exp)
-        {
-            Toast.makeText(this,"Error" + exp.getMessage(),Toast.LENGTH_SHORT).show();
-        }
-
+//        spnCategory.setAdapter(adapter1);
+//        spnCategory.setEnabled(false);
+//
+//        txtDate =findViewById(R.id.date_or_days);
+//        txtTime=findViewById(R.id.txtTime);
+//
+//        edTittle=findViewById(R.id.edtTitle);
+//        edtDescription=findViewById(R.id.edDescription);
+//        txtlocation=findViewById(R.id.edtLocation);
+//
+//        try
+//        {
+//            if(!getIntent().getExtras().getString("date").equals(""))
+//            {
+//                edTittle.setText(getIntent().getExtras().getString("title"));
+//                edtDescription.setText(getIntent().getExtras().getString("description"));
+//                txtTime.setText(getIntent().getExtras().getString("time"));
+//                txtAM_PM.setText(getIntent().getExtras().getString("am_pm"));
+//                txtlocation.setText(getIntent().getExtras().getString("location"));
+//                txtDate.setText(getIntent().getExtras().getString("date"));
+//            }
+//        }
+//        catch (Exception exp)
+//        {
+//            Toast.makeText(this,"Error" + exp.getMessage(),Toast.LENGTH_SHORT).show();
+//        }
+//
+//    }
     }
 }
